3ac5b344d6269cd913decc83b818c536eea56e13
VamaRanderia <vama.randeria@gmail.com>
2026-06-24 12:42:05 +0530

    Initial MERN setup

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+console.log("URI:", process.env.MONGO_URI);
+console.log(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
