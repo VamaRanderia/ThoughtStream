0dd00b1f28b521fec2db85bd2fc46c81cc9a45c2
VamaRanderia <vama.randeria@gmail.com>
2026-06-24 15:56:50 +0530

    Add update user API

--- backend/server.js
+++ backend/server.js
@@ -39,9 +39,37 @@ app.post("/users", async (req, res) => {
     }
 });
 
+// GET by _id route
 app.get("/users/:id", async (req, res) => {
     try {
         const user = await User.findById(req.params.id);
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        res.json(user);
+    }
+    catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+});
+
+// PUT route
+app.put("/users/:id", async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {
+                new: true,
+                runValidators: true
+            }
+        );
 
         if (!user) {
             return res.status(404).json({
