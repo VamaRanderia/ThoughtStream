e893e181a1addc30691c34850b2252e7c83899e0
VamaRanderia <vama.randeria@gmail.com>
2026-06-29 16:36:41 +0530

    Add Signup page UI with controlled form

---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+
+function Signup() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(formData);
+
+    // Backend connection will be added next
+  };
+
+  return (
+    <div>
+      <h1>Signup</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter Name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+
+        <br /><br />
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter Email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+
+        <br /><br />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter Password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+
+        <br /><br />
+
+        <button type="submit">
+          Signup
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Signup;
