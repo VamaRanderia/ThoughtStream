dbb30bcdf22968f8608b385a64235f392c7ae2a2
VamaRanderia <vama.randeria@gmail.com>
2026-06-29 13:11:16 +0530

    Implement JWT-based user login

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,16 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
+const generateToken = (id) => {
+    return jwt.sign(
+        { id },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "7d"
+        }
+    );
+};
+
 const registerUser = async (req, res) => {
     try {
 
@@ -40,6 +50,39 @@ const registerUser = async (req, res) => {
     }
 };
 
+const loginUser = async (req, res) => {
+    try {
+
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+}
+    const isMatch = await bcrypt.compare(password, user.password);
+
+if (!isMatch) {
+    return res.status(401).json({
+        message: "Invalid credentials"
+    });
+}
+    const token = generateToken(user._id);
+    res.status(200).json({
+    message: "Login successful",
+    token
+});
+    } catch (error) {
+
+        res.status(500).json({
+            message: error.message
+        });
+
+    }
+};
+
 module.exports = {
-    registerUser
+    registerUser,
+    loginUser
 };
