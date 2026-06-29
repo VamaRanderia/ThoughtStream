99520f954d972b47de683e627a9211ff2b8e8d7b
VamaRanderia <vama.randeria@gmail.com>
2026-06-29 12:25:14 +0530

    Implemented user registration with ncrypt and auth routes

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,12 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const User = require("./models/User");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 console.log("URI:", process.env.MONGO_URI);
 console.log(process.env.MONGO_URI);
@@ -18,19 +20,6 @@ app.get("/users", async (req, res) => {
     try {
         const users = await User.find();
         res.json(users);
-    }
-    catch (error) {
-        res.status(500).json({
-            message: error.message
-        });
-    }
-});
-
-app.post("/users", async (req, res) => {
-    try {
-        const user = await User.create(req.body);
-
-        res.status(201).json(user);
     }
     catch (error) {
         res.status(500).json({
